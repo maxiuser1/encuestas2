f6315ab50ae99e0144273b55dc7106af915b37ec
Jose.Calderon <jose.calderon@tranzact.net>
2021-10-11 18:46:53 -0500

    incluye guid

--- api/postgerencias/index.ts
+++ api/postgerencias/index.ts
@@ -1,17 +1,24 @@
-import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { AzureFunction, Context, HttpRequest } from '@azure/functions';
+import { getGuid } from '../common/Utils';
 
-const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+const httpTrigger: AzureFunction = async function (
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const todoItem = {
+    id: getGuid(),
+    nombre: 'test',
+    apellido: 'test2',
+    compania: 'bci',
+  };
 
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+  context.bindings.gerencia = todoItem;
+  context.res = {
+    status: 201,
+    body: '',
+  };
 
+  context.done();
 };
 
 export default httpTrigger;
