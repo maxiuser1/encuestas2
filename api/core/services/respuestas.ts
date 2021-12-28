88f30464efb8d577808450f69061fdec9555e28f
Jose.Calderon <jose.calderon@tranzact.net>
2021-12-28 15:50:09 -0500

    fix servicios

--- api/core/services/respuestas.ts
+++ api/core/services/respuestas.ts
@@ -22,60 +22,80 @@ export class Respuestas {
         let respuestas: Respuesta[] = [];
 
         for (const gerencia of gerencias as Array<Gerencia>) {
-            for (const gerenciaRd of gerencia.gerenciasrd as Array<Gerenciard>) {
-                for (const subgerencia of gerenciaRd.subgerencias as Array<Subgerencia>) {
-                    for (const area of subgerencia.areas as Array<Area>) {
-                        const servicios = area.servicios;
-                        procesarServicio(
-                            servicios,
-                            gerencia,
-                            gerenciaRd,
-                            subgerencia,
-                            area
-                        );
+            if (gerencia.gerenciasrd) {
+                for (const gerenciaRd of gerencia.gerenciasrd as Array<Gerenciard>) {
+                    if (gerenciaRd.subgerencias) {
+                        for (const subgerencia of gerenciaRd.subgerencias as Array<Subgerencia>) {
+                            if (subgerencia.areas) {
+                                for (const area of subgerencia.areas as Array<Area>) {
+                                    const servicios = area.servicios;
+                                    procesarServicio(
+                                        servicios,
+                                        gerencia,
+                                        gerenciaRd,
+                                        subgerencia,
+                                        area
+                                    );
+                                }
+                            }
+
+                            const servicios = subgerencia.servicios;
+                            procesarServicio(
+                                servicios,
+                                gerencia,
+                                gerenciaRd,
+                                subgerencia,
+                                null
+                            );
+                        }
+                    }
+
+                    if (gerenciaRd.areas) {
+                        for (const area of gerenciaRd.areas as Array<Area>) {
+                            const servicios = area.servicios;
+                            procesarServicio(
+                                servicios,
+                                gerencia,
+                                gerenciaRd,
+                                null,
+                                area
+                            );
+                        }
+                    }
+                }
+            }
+
+            if (gerencia.subgerencias) {
+                for (const subgerencia of gerencia.subgerencias as Array<Subgerencia>) {
+                    if (subgerencia.areas) {
+                        for (const area of subgerencia.areas as Array<Area>) {
+                            const servicios = area.servicios;
+                            procesarServicio(
+                                servicios,
+                                gerencia,
+                                null,
+                                subgerencia,
+                                area
+                            );
+                        }
                     }
 
                     const servicios = subgerencia.servicios;
                     procesarServicio(
                         servicios,
                         gerencia,
-                        gerenciaRd,
+                        null,
                         subgerencia,
                         null
                     );
                 }
-
-                for (const area of gerenciaRd.areas as Array<Area>) {
-                    const servicios = area.servicios;
-                    procesarServicio(
-                        servicios,
-                        gerencia,
-                        gerenciaRd,
-                        null,
-                        area
-                    );
-                }
             }
 
-            for (const subgerencia of gerencia.subgerencias as Array<Subgerencia>) {
-                for (const area of subgerencia.areas as Array<Area>) {
+            if (gerencia.areas) {
+                for (const area of gerencia.areas as Array<Area>) {
                     const servicios = area.servicios;
-                    procesarServicio(
-                        servicios,
-                        gerencia,
-                        null,
-                        subgerencia,
-                        area
-                    );
+                    procesarServicio(servicios, gerencia, null, null, area);
                 }
-
-                const servicios = subgerencia.servicios;
-                procesarServicio(servicios, gerencia, null, subgerencia, null);
-            }
-
-            for (const area of gerencia.areas as Array<Area>) {
-                const servicios = area.servicios;
-                procesarServicio(servicios, gerencia, null, null, area);
             }
         }
 
@@ -86,72 +106,75 @@ export class Respuestas {
             subgerencia: Subgerencia,
             area: Area
         ) {
-            for (const servicio of servicios as Array<Servicio>) {
-                if (servicio.definidos && servicio.definidos.length > 0) {
-                    for (const evaluador of servicio.definidos) {
-                        let evaluacion: Evaluacion = {
-                            gerenciaId: gerencia?.id,
-                            gerencia: gerencia?.nombre,
-                            gerenciardId: gerenciaRd?.id,
-                            gerenciard: gerenciaRd?.nombre,
-                            subgerenciaId: subgerencia?.id,
-                            subgerencia: subgerencia?.nombre,
-                            areaId: area?.id,
-                            area: area?.nombre,
-                            servicioId: servicio.id,
-                            servicio: servicio.nombre,
-                            preguntas: [],
-                            responsableId: servicio.responsable?.id,
-                            responsable: servicio.responsable?.name,
-                        };
-
-                        for (let cadaPregunta of encuesta.preguntas) {
-                            if (
-                                cadaPregunta.tipos.some(
-                                    (t) => t == servicio.tipo
-                                )
-                            )
-                                evaluacion.preguntas.push({
-                                    glosa: cadaPregunta.glosa,
-                                    preguntaId: cadaPregunta.id,
-                                    valor: '',
-                                });
-                        }
-
-                        if (
-                            respuestas.some(
-                                (t) => t.evaluador.id == evaluador.id
-                            )
-                        ) {
-                            let respuesta =
-                                respuestas[
-                                    respuestas.findIndex(
-                                        (t) => t.evaluador.id == evaluador.id
-                                    )
-                                ];
-                            respuesta.evaluaciones.push(evaluacion);
-                        } else {
-                            let respuesta: Respuesta = {
-                                id: getGuid(),
-                                evaluador: {
-                                    id: evaluador.id,
-                                    name: evaluador.name,
-                                    email:
-                                        envi == 'PROD'
-                                            ? evaluador.email
-                                            : 'pe.jose.calderon@gmail.com',
-                                },
-                                evaluaciones: [],
-                                campana: {
-                                    id: campana.id,
-                                    nombre: campana.nombre,
-                                    fechaLimite: campana.fechaLimite,
-                                    encuestaId: campana.encuestaId,
-                                    estado: campana.estado,
-                                },
+            if (servicios) {
+                for (const servicio of servicios as Array<Servicio>) {
+                    if (servicio.definidos && servicio.definidos.length > 0) {
+                        for (const evaluador of servicio.definidos) {
+                            let evaluacion: Evaluacion = {
+                                gerenciaId: gerencia?.id,
+                                gerencia: gerencia?.nombre,
+                                gerenciardId: gerenciaRd?.id,
+                                gerenciard: gerenciaRd?.nombre,
+                                subgerenciaId: subgerencia?.id,
+                                subgerencia: subgerencia?.nombre,
+                                areaId: area?.id,
+                                area: area?.nombre,
+                                servicioId: servicio.id,
+                                servicio: servicio.nombre,
+                                preguntas: [],
+                                responsableId: servicio.responsable?.id,
+                                responsable: servicio.responsable?.name,
                             };
-                            respuesta.evaluaciones.push(evaluacion);
-                            respuestas.push(respuesta);
+
+                            for (let cadaPregunta of encuesta.preguntas) {
+                                if (
+                                    cadaPregunta.tipos.some(
+                                        (t) => t == servicio.tipo
+                                    )
+                                )
+                                    evaluacion.preguntas.push({
+                                        glosa: cadaPregunta.glosa,
+                                        preguntaId: cadaPregunta.id,
+                                        valor: '',
+                                    });
+                            }
+
+                            if (
+                                respuestas.some(
+                                    (t) => t.evaluador.id == evaluador.id
+                                )
+                            ) {
+                                let respuesta =
+                                    respuestas[
+                                        respuestas.findIndex(
+                                            (t) =>
+                                                t.evaluador.id == evaluador.id
+                                        )
+                                    ];
+                                respuesta.evaluaciones.push(evaluacion);
+                            } else {
+                                let respuesta: Respuesta = {
+                                    id: getGuid(),
+                                    evaluador: {
+                                        id: evaluador.id,
+                                        name: evaluador.name,
+                                        email:
+                                            envi == 'PROD'
+                                                ? evaluador.email
+                                                : 'pe.jose.calderon@gmail.com',
+                                    },
+                                    evaluaciones: [],
+                                    campana: {
+                                        id: campana.id,
+                                        nombre: campana.nombre,
+                                        fechaLimite: campana.fechaLimite,
+                                        encuestaId: campana.encuestaId,
+                                        estado: campana.estado,
+                                    },
+                                };
+                                respuesta.evaluaciones.push(evaluacion);
+                                respuestas.push(respuesta);
+                            }
                         }
                     }
                 }
